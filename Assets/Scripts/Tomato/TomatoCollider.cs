25fc5300ab78bb7087d0c628393fcb7af74664e0
Saúl Cañedo Castillo <saul.canedocast@hotmail.com>
2022-01-18 23:27:17 -0800

    chore: agregar rebote de pelota

--- Assets/Scripts/Tomato/TomatoCollider.cs
+++ Assets/Scripts/Tomato/TomatoCollider.cs
@@ -7,15 +7,20 @@ public class TomatoCollider : MonoBehaviour
     public bool activated = false;
     // Start is called before the first frame update
     private SphereCollider tomatoColl;
+    private MeshCollider tomatoMesh;
     void Start()
     {
         tomatoColl = GetComponent<SphereCollider>();
+        tomatoMesh = GetComponent<MeshCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(activated == true)
+        {
             tomatoColl.enabled = true;
+            tomatoMesh.enabled = true;
+        }
     }
 }
